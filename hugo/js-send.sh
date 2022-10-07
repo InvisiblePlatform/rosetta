b2c68b1cdfc3f505a724ce68ad070d649aab12fd
NfN Orange <orange@ff4500.red>
2022-10-07 12:45:51 +0100

    Saving again

---
+++ hugo/js-send.sh
@@ -0,0 +1,1 @@
+scp static/js/single.js ixt_testorange@ssh.phx.nearlyfreespeech.net:js/single.js
--- hugo/static/js/single.js
+++ hugo/static/js/single.js
@@ -1,62 +1,120 @@
-let allLinks = document.querySelectorAll('a');
+function send_message(type, data){
+    var msg = {
+        type: type,
+        data: data
+    };
+    if (parent){
+	    try{
+            parent.postMessage(msg, "*");
+	    } catch(e){
+	    	console.log(e);
+	    }
+    } else {
+        console.log("parent not found");
+    }
+}
 
-allLinks.forEach(el => {
-  if (el.href.toString().includes("wikipedia.org") && el.target != "_blank" && !el.classList.contains("source")){
-      href = el.href;
-      org_html = el.innerHTML;
-      parent = el.parentElement;
-      var new_el = document.createElement("span");
-      new_el.innerHTML = "<span class='hover-anchor' onmouseover='loadWikiInfo(this)'><a target='_blank' href=\"" + el.href + "\" >" + org_html + "</a><span class='hover-pop dot'>This will be a little pop-out wikipedia explanation that appears on hover</span></span>"
-      parent.replaceChild(new_el,el);
-}});
-
-let hoverAnchors = document.querySelectorAll('.hover-anchor');
+// let allLinks = document.querySelectorAll('a');
+// 
+// allLinks.forEach(el => {
+//   if (el.href.toString().includes("wikipedia.org") && el.target != "_blank" && !el.classList.contains("source")){
+//       href = el.href;
+//       org_html = el.innerHTML;
+//       parentW = el.parentElement;
+//       var new_el = document.createElement("span");
+//       new_el.innerHTML = "<span class='hover-anchor' onmouseover='loadWikiInfo(this)'><a target='_blank' href=\"" + el.href + "\" >" + org_html + "</a><span class='hover-pop dot'>This will be a little pop-out wikipedia explanation that appears on hover</span></span>"
+//       parentW.replaceChild(new_el,el);
+// }});
+// 
+// let hoverAnchors = document.querySelectorAll('.hover-anchor');
 let wW = window.innerWidth;
 
-hoverAnchors.forEach(el => {
-  let elPos = el.getBoundingClientRect();
-  let sixty = wW * 60 / 100;
-  let hoverPop = el.querySelector('.hover-pop');
-
-  if (elPos.left > sixty) {
-    hoverPop.classList.remove('dot');
-    hoverPop.classList.add('dot-right');
-  }
-})
+// hoverAnchors.forEach(el => {
+//     return;
+//   let elPos = el.getBoundingClientRect();
+//   let sixty = wW * 60 / 100;
+//   let hoverPop = el.querySelector('.hover-pop');
+// 
+//   if (elPos.left > sixty) {
+//     hoverPop.classList.remove('dot');
+//     hoverPop.classList.add('dot-right');
+//   }
+// })
 let backButton = document.getElementById('backButton');
-let resetBack = function(x){
-    x.style.visibility = 'hidden';
-    backButton.style.visibility = 'hidden';
-    backButton.setAttribute("onclick", '');
+let closeButton = document.getElementById('closeButton');
+closeButton.setAttribute('onclick', 'closeIV()');
+
+let closeIV = function(){
+    send_message("IVClose", "closeButton");
+};
+
+
+let resetBack = function(){
+    settings.style.visibility = 'hidden';
+    networkGraph.style.visibility = 'hidden';
+    backButton.setAttribute("onclick", 'justSendBack()');
+    coName.style.visibility = 'visible';
 }
 let setBack = function(x){
-    backButton.style.visibility = 'visible';
     backButton.setAttribute("onclick", x);
 }
 
 let settings = document.getElementById('settings');
 let networkGraph = document.getElementById('graph-container');
+let infoCard = document.getElementById('wikipedia-infocard-frame');
+let wikipediaPage = document.getElementById('wikipedia-first-frame');
+let coName = document.getElementsByClassName('co-name')[0];
 
+let loadWikipediaPage = function(x) {
+    wikipediaPage.classList.add('expanded');
+    send_message("IVClicked", "wikipedia-first-frame");
+    setBack('closeWikipediaPage()');
+}
+
+let loadProfileCard = function(x) {
+    infoCard.classList.add('expanded');
+    send_message("IVClicked", "wikipedia-infocard-frame");
+    setBack('closeInfoCard()');
+}
 let loadSettings = function(x) {
     settings.style.visibility = 'visible';
+    coName.style.visibility = 'hidden';
+    send_message("IVClicked", "settings");
     setBack('closeSettings()');
 }
 
 let loadNetworkGraph = function(x) {
     networkGraph.style.visibility = 'visible';
+    window.scrollTo(0,0);
+    document.getElementsByTagName('content');
+    send_message("IVClicked", "network");
     setBack('closeNetworkGraph()');
 }
 
+let closeWikipediaPage = function(x){
+    wikipediaPage.classList.remove('expanded');
+    resetBack();
+}
+let closeInfoCard = function(x){
+    infoCard.classList.remove('expanded');
+    resetBack();
+}
 let closeNetworkGraph = function(x){
     networkGraph.style.visibility = 'hidden';
-    resetBack(networkGraph);
+    send_message("IVClicked", "back");
+    resetBack();
+}
+
+let justSendBack = function(x) {
+    send_message("IVClicked", "back");
 }
 
 let closeSettings = function(x) {
-    resetBack(settings);
+    resetBack();
 }
 
 let loadWikiInfo = function(x) {
+    return;
 	let url = x.firstElementChild.getAttribute("href");	
 	if (url != "null"){
 		let term = url.replace("https://en.wikipedia.org/wiki/", ""); 
@@ -78,40 +136,60 @@ let loadWikiInfo = function(x) {
 	x.removeAttribute("onmouseover");
   }
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "";
-    }
-  }); 
-} 
-
+document.addEventListener("DOMContentLoaded", function(){
 document.addEventListener('mouseup', function(event){
+    console.log("IVCLICKED", event.target.parentElement.id);
+    if (event.target.classList.contains('sectionTitle')){
+        send_message("IVClicked", event.target.parentElement.id);
+    }
+    if (event.target.matches('#backButton')){
+        send_message("IVClicked", event.target.parentElement.id);
+    }
     if (event.target.matches('#profile-card')){
         send_message("biggen", "big");
         console.log("bigg");
     }
-})
-function send_message(type, data)
-{
-    var msg = {
-        type: type,
-        data: data
-    };
-    parent.postMessage(msg, "*");
-}
+})},false);
+// {value: items[it].value, label: items[it].innerHTML}
+var defaultOrder = [
+    { value:"wikipedia-first-frame", label: "Wikipedia"},
+    { value:"networkgraph", label: "Network Graph and Company Info"},
+    { value:"wikidata-small", label: "Political Alignment"},
+    { value:"mbfc-header", label: "Media Bias"},
+    { value:"trust-pilot", label: "Trust Pilot"},
+    { value:"yahoo", label: "ESG Risk"},
+    { value:"isin", label: "Low Carbon Transition"},
+    { value:"isin", label: "Food & Agriculture Sustainability"},
+    { value:"isin", label: "Social & Sustainable Development"},
+    { value:"goodonyou", label: "Ethical Sourcing"},
+    { value:"bcorp", label: "B corp"},
+    { value:"tosdr-link", label: "Privacy"},
+    { value:"glassdoor", label: "Employee Rating"},
+    { value:"similar-site-wrapper", label: "Similar Websites"},
+    { value:"social-wikidata", label: "Social Media + Email"},
+];
+var propertyOrder = localStorage.IVPropertyOrder ? JSON.parse(localStorage.IVPropertyOrder) : defaultOrder;
+slist(document.getElementById("sortlist"));
 function slist (target) {
   // (A) SET CSS + GET ALL LIST ITEMS
   target.classList.add("slist");
   let items = target.getElementsByTagName("li"), current = null;
   for (let x = 0; x < propertyOrder.length; x++){
-    items[x].innerHTML = propertyOrder[x];
+    let value = propertyOrder[x].value;
+    items[x].setAttribute("value", value);
+    items[x].innerHTML = propertyOrder[x].label;
+    if (value == "networkgraph"){
+        if (document.getElementById("graph-box")){
+            document.getElementById("graph-box").style.order = x + 5;
+        }
+        if (document.getElementById("wikipedia-infocard-frame")){
+            document.getElementById("wikipedia-infocard-frame").style.order = x + 5;
+        }
+    }
+    if (document.getElementById(value)){
+        document.getElementById(value).style.order = x + 5;
+    }
+
   };
 
 
@@ -164,11 +242,21 @@ function slist (target) {
       }
       let outItems = [];
       for (let it=0; it<items.length; it++){
-	outItems.push(items[it].innerHTML);
+          outItems.push({value: items[it].getAttribute("value"), label: items[it].innerHTML});
+            let value = items[it].getAttribute("value");
+            if (value == "networkgraph"){
+                if (document.getElementById("graph-box")){
+                    document.getElementById("graph-box").style.order = it + 5;
+                }
+                if (document.getElementById("wikipedia-infocard-frame")){
+                    document.getElementById("wikipedia-infocard-frame").style.order = it + 5;
+                }
+            }
+            if (document.getElementById(value)){
+                document.getElementById(value).style.order = it + 5;
+            }
       }
-      localstorage.set({
-          "propertyOrder": outItems
-      });
+      localStorage.IVPropertyOrder = JSON.stringify(outItems);
       console.log(outItems);
     };
   }
