dd42c709c22e25e6207a1ce8b34e88a8134ba250
NfN Orange <orange@ff4500.red>
2023-02-11 17:38:21 +0000

    Adding a couple of worldbenchmark benchmarks

--- data_collection/static/static_lookup_generate.sh
+++ data_collection/static/static_lookup_generate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-rm static/document_isin.list
-jq -r ".[].ISIN" static/2022-January_Just-Transition-Assessment_Datasheet_JustTransitionData.json | sed -e 's/^/static\/2022-January_Just-Transition-Assessment_Datasheet_JustTransitionData.json:/g' > static/document_isin.list
-jq -r ".[].ISIN" static/2022-January_Just-Transition-Assessment_Datasheet_SocialData.json | sed -e 's/^/static\/2022-January_Just-Transition-Assessment_Datasheet_SocialData.json:/g' >> static/document_isin.list
-jq -r ".[].ISIN" static/Food-and-Agriculture-Benchmark-detailed-scoring-sheet-2021-2.json | sed -e 's/^/static\/Food-and-Agriculture-Benchmark-detailed-scoring-sheet-2021-2.json:/g' >> static/document_isin.list
-jq -r '.[]."Company ISIN"' static/WBA_Social_Transformation_Baseline_Data_JAN_2022_overall_scores.json | sed -e "s/^/static\/WBA_Social_Transformation_Baseline_Data_JAN_2022_overall_scores.json:/g" >> static/document_isin.list
-sed -i 's/^/"/g;s/$/"/g' static/document_isin.list
+# jq 'reduce .[] as $item ({}; .[$item.ISIN]=$item)' 2022_FinancialBenchmark.json
+rm document_isin.list
+jq -r ".[].ISIN" 2022-January_Just-Transition-Assessment_Datasheet_JustTransitionData.json | sed -e 's/^/static\/2022-January_Just-Transition-Assessment_Datasheet_JustTransitionData.json:/g' > document_isin.list
+jq -r ".[].ISIN" 2022-January_Just-Transition-Assessment_Datasheet_SocialData.json | sed -e 's/^/static\/2022-January_Just-Transition-Assessment_Datasheet_SocialData.json:/g' >> document_isin.list
+jq -r ".[].ISIN" Food-and-Agriculture-Benchmark-detailed-scoring-sheet-2021-2.json | sed -e 's/^/static\/Food-and-Agriculture-Benchmark-detailed-scoring-sheet-2021-2.json:/g' >> document_isin.list
+jq -r ".[].ISIN" 2022_FinancialBenchmark.json | sed -e 's/^/static\/2022_FinancialBenchmark.json:/g' >> document_isin.list
+jq -r ".[].ISIN" 2022-TransportBenchmark.json | sed -e 's/^/static\/2022-TransportBenchmark.json:/g' >> document_isin.list
+jq -r '.[]."Company ISIN"' WBA_Social_Transformation_Baseline_Data_JAN_2022_overall_scores.json | sed -e "s/^/static\/WBA_Social_Transformation_Baseline_Data_JAN_2022_overall_scores.json:/g" >> document_isin.list
+sed -i 's/^/"/g;s/$/"/g' document_isin.list
